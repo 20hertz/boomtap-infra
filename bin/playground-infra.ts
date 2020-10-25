85c918b8fd04b822ce416f413fedfe79e20d06d9
Stéphane B <stillfury@hotmail.com>
2020-10-25 17:30:06 -0400

    create bucket resource + deployment

--- bin/playground-infra.ts
+++ bin/playground-infra.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { PlaygroundInfraStack } from '../lib/playground-infra-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { PlaygroundInfraStack } from "../lib/playground-infra-stack";
 
 const app = new cdk.App();
-new PlaygroundInfraStack(app, 'PlaygroundInfraStack');
+new PlaygroundInfraStack(app, "PlaygroundInfraStack", {
+  env: {
+    region: "ca-central-1",
+  },
+});
--- lib/playground-infra-stack.ts
+++ lib/playground-infra-stack.ts
@@ -1,9 +1,24 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 
 export class PlaygroundInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "stephane-static-website", {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+    });
+
+    const deployment = new s3Deployment.BucketDeployment(
+      this,
+      "deployStaticWebsite",
+      {
+        sources: [s3Deployment.Source.asset("../midi-proto/dist")],
+        destinationBucket: bucket,
+      }
+    );
   }
 }
