52514e7a68c57906a57d254eae21d93e3faa6560
Stéphane B <stillfury@hotmail.com>
2020-10-25 19:25:30 -0400

    add cloudfront distribution

--- lib/playground-infra-stack.ts
+++ lib/playground-infra-stack.ts
@@ -1,24 +1,41 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
 
 export class PlaygroundInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, "stephane-static-website", {
+    const assetBucket = new s3.Bucket(this, "stephane-static-website", {
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       websiteIndexDocument: "index.html",
     });
 
-    const deployment = new s3Deployment.BucketDeployment(
+    const distribution = new cloudfront.CloudFrontWebDistribution(
       this,
-      "deployStaticWebsite",
+      "SiteDistribution",
       {
-        sources: [s3Deployment.Source.asset("../midi-proto/dist")],
-        destinationBucket: bucket,
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: assetBucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
       }
     );
+
+    new cdk.CfnOutput(this, "DistributionDomainName", {
+      value: distribution.distributionDomainName,
+    });
+
+    new s3Deployment.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3Deployment.Source.asset("../midi-proto/dist")],
+      destinationBucket: assetBucket,
+      distribution: distribution,
+    });
   }
 }
