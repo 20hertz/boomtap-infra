463b0dc4214ed64d9d4180658071c3db73da3c81
Stéphane B <stillfury@hotmail.com>
2020-10-26 21:03:05 -0400

    add public certificate to cloudfront distribution

--- test/playground-infra.test.ts
+++ test/playground-infra.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as PlaygroundInfra from '../lib/playground-infra-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as PlaygroundInfra from "../lib/playground-infra-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new PlaygroundInfra.PlaygroundInfraStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new PlaygroundInfra.FrontEndStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
