d069dba393719b55db92685362ab1934ac521c11
Stéphane B <stillfury@hotmail.com>
2021-02-14 19:43:56 -0500

    Upgrade dependencies + move to Yarn 2 + add new env variables

--- bin/boomtap-infra.ts
+++ bin/boomtap-infra.ts
@@ -3,9 +3,13 @@ import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { FrontEndStack } from "../lib/boomtap-infra-stack";
 
-const certificateArn =
-  "arn:aws:acm:us-east-1:770668172371:certificate/913a571a-e758-48c7-a6d2-3b3c5191bf91";
+const envProd = {
+  certificateArn:
+    "arn:aws:acm:us-east-1:770668172371:certificate/913a571a-e758-48c7-a6d2-3b3c5191bf91",
+  domainName: "boomtap.io",
+  env: "prod",
+};
 
 const app = new cdk.App();
 
-new FrontEndStack(app, "FrontEndStack", { certificateArn });
+new FrontEndStack(app, "FrontEndStackProd", envProd);
