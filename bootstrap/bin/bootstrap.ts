33063ace26a8a10032035483bc08aad2759aaea1
Stéphane B <stillfury@hotmail.com>
2021-12-22 16:17:16 -0500

    build: install dependencies

--- bootstrap/bin/bootstrap.ts
+++ bootstrap/bin/bootstrap.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { BootstrapStack } from '../lib/bootstrap-stack';
+import { App, DefaultStackSynthesizer } from "aws-cdk-lib";
+import { BootstrapStack } from "../lib/bootstrap-stack";
 
-const app = new cdk.App();
-new BootstrapStack(app, 'BootstrapStack');
+const app = new App();
+new BootstrapStack(app, "BootstrapStack", {
+  env: {
+    region: "ca-central-1",
+  },
+  synthesizer: new DefaultStackSynthesizer({
+    // Specified at the bootstrap time. Checkout package.json "bootstrap" script.
+    qualifier: "bootstrap",
+  }),
+});
