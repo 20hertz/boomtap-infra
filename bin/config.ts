b113413503009148ea36483bd56c79ffefa8ff41
Stéphane B <stillfury@hotmail.com>
2023-10-14 16:31:54 -0400

    feat: configure for prod

--- bin/config.ts
+++ bin/config.ts
@@ -1,5 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-const supportedEnvironments = ["staging"] as const;
+const supportedEnvironments = ["backstage", "production"] as const;
 
 type SupportedEnvironments = (typeof supportedEnvironments)[number];
 
