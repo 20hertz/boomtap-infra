b7def73566f4f03d21af4a4c2a1ee9cf77b21ea9
Stéphane <20hertz@users.noreply.github.com>
2023-10-14 21:00:37 -0400

    feat: constrain Github OIDC to specific environment (#89)

--- bin/main.ts
+++ bin/main.ts
@@ -3,12 +3,21 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { GitHubStack } from "../lib/OIDC/github-stack";
 import { FileStorageStack } from "../lib/storage";
+import { getContext } from "./config";
 
 const app = new cdk.App();
 
+const { environment } = getContext(app);
+
 new GitHubStack(app, "GitHubOpenIDConnect", {
   deployRole: "GitHubDeployRole",
-  repositoryConfig: [{ owner: "20hertz", repo: "boomtap-infra" }],
+  repositoryConfig: [
+    {
+      owner: "20hertz",
+      repo: "boomtap-infra",
+      filter: `environment:${environment}`,
+    },
+  ],
 });
 
 new FileStorageStack(app, "FileStorageStack");
